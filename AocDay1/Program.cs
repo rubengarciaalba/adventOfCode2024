d6ac33d7adf6cdb289c58cc40f9090a2f47fb983
Ruben Garcia <rubengarciaalba@gmail.com>
2025-10-14 12:27:04 +0200

    Created project and solution for advent of code day 1 challenge

---
+++ AocDay1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AocDay1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var origins = new List<int>() { 3, 4, 2, 1, 3, 3 };
+            var destinations = new List<int>() { 4, 3, 5, 3, 9, 3 };
+
+            PrintDistance(origins, destinations);
+        }
+
+        private static void PrintDistance(List<int> origins, List<int> destinations)
+        {
+            var distance = 0;
+
+            origins.Sort();
+            destinations.Sort();
+
+            if (origins.Count != destinations.Count)
+            {
+                Console.WriteLine("The given lists don't have the same length");
+                return;
+            }
+
+            for (int i = 0; i < origins.Count; i++)
+            {
+                if (origins[i] > destinations[i])
+                {
+                    distance += origins[i] - destinations[i];
+
+                    continue;
+                }
+
+                distance += destinations[i] - origins[i];
+            }
+
+            Console.WriteLine($"The total distance is: {distance}");
+        }
+    }
+}
